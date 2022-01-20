d6e280f7448b22422e427b2ceb36d03c1aa0a2d8
root <root@localhost.localdomain>
2022-01-20 14:32:47 -0500

    u

--- jenkins/setup/backend.tf
+++ jenkins/setup/backend.tf
@@ -2,6 +2,8 @@ terraform {
   backend "s3" {
     bucket = "node-aws-artifact"
     key    = "node-aws-artifact.tfstate"
+    access_key = "AKIAVSRNOMAYZ4DG5UXI"
+    secret_key = "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"
     region = "us-west-2"
   }
 }
