48eb479a1084806fd470a63e9406b982fdad110d
root <root@localhost.localdomain>
2022-01-21 06:59:34 -0500

    Update

--- jenkins/setup/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "node-aws-jenkins-terraform"
-    key    = "jenkins.terraform.tfstate"
-    region = "eu-west-1"
-  }
-}
--- terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "node-aws-jenkins-terraform"
-    key = "node-aws-jenkins-terraform.tfstate"
-    region = "eu-west-1"
-  }
-}
